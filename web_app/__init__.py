63bef22ab7d643612545be5fc5b1994aa96051b0
rowaishanna <rowaishanna@gmail.com>
2020-06-06 01:54:15 -0400

    added twitter service, basilica service and associated route and html page

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -3,8 +3,9 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.twitter_routes import twitter_routes
 
-DATABASE_URI = "sqlite:///web_app_99.db" 
+DATABASE_URI = "sqlite:///twitoff_rowais.db" 
 
 def create_app():
     app = Flask(__name__)
@@ -15,7 +16,7 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
-
+    app.register_blueprint(twitter_routes)
     return app
 
 if __name__ == "__main__":
